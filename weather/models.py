ee74bab24f687f9ee4fb8007aeca9c7754cc6940
fdanci <florin.danci96@gmail.com>
2020-11-30 19:04:47 +0200

    Increase date field max size

--- weather/models.py
+++ weather/models.py
@@ -5,7 +5,7 @@ class Forecast(models.Model):
     """An entity that holds information 5 days worth"""
 
     forecast = models.CharField(max_length=30000, default='None')
-    date = models.CharField(max_length=10, default='None')
+    date = models.CharField(max_length=20, default='None')
     location = models.CharField(max_length=20, default='None')
 
     class Meta:
