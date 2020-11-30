7ea04396359f66da561e5b1195566c5a529e3d1d
fdanci <florin.danci96@gmail.com>
2020-11-30 14:27:59 +0200

    Add hourly forecast request, change max min temperature style

--- weather/weather_sources/source_1.py
+++ weather/weather_sources/source_1.py
@@ -6,6 +6,7 @@ class Source1:
     """
     Source: 'https://developer.accuweather.com/'
     """
+
     @staticmethod
     def today(location_id):
         """
@@ -14,7 +15,8 @@ class Source1:
         :return: The forecast in Json format.
         """
         url = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + location_id + "" \
-              "?apikey=" + config('ACC_WEATHER_KEY') + "&language=ro&details=true&metric=true"
+                                                                                            "?apikey=" + config(
+            'ACC_WEATHER_KEY') + "&language=ro&details=true&metric=true"
 
         response = requests.request("GET", url)
 
@@ -32,7 +34,8 @@ class Source1:
         :return: The forecast in Json format.
         """
         url = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + location_id + "" \
-              "?apikey=" + config('ACC_WEATHER_KEY') + "&language=ro&details=true&metric=true"
+                                                                                            "?apikey=" + config(
+            'ACC_WEATHER_KEY') + "&language=ro&details=true&metric=true"
 
         response = requests.request("GET", url)
 
@@ -40,4 +43,23 @@ class Source1:
         if response.status_code == 503:
             raise Exception('A fost depășit numărul de cereri pe ziua de azi')
 
+        return response
+
+    @staticmethod
+    def forecast_12hours(location_id):
+        """
+        Read weather forecast for 12 hours.
+
+        :return: The forecast in Json format.
+        """
+        url = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/" + location_id + "" \
+                                                                                               "?apikey=" + config(
+            'ACC_WEATHER_KEY') + "&language=ro&details=true&metric=true"
+
+        response = requests.request("GET", url)
+
+        # Raise exception if no more requests for today!
+        if response.status_code == 503:
+            raise Exception('A fost depășit numărul de cereri pe ziua de azi')
+        print(response.text)
         return response
