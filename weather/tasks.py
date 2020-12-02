ca5f7f1f889b1d7ac4b4af342097ec6b0ee1407d
fdanci <florin.danci96@gmail.com>
2020-12-02 19:12:44 +0200

    Add app email

--- weather/tasks.py
+++ weather/tasks.py
@@ -2,11 +2,13 @@ from celery import shared_task
 
 from django.core.mail import send_mail
 
+from decouple import config
+
 
 @shared_task
 def send_email():
-    send_mail('Celey Task Worked!',
+    send_mail('Celery Task Worked!',
               'This is the proof the task worked!',
-              'florin.danci96@gmail.com',
+              config('EMAIL'),
               ['florin.danci96@gmail.com'])
     return None
