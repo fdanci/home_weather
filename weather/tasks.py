f88d4fb357f2bb7be6fab8322dd54e264128a13a
fdanci <florin.danci96@gmail.com>
2020-12-03 11:24:48 +0200

    Stop heroku server from spamming me

--- weather/tasks.py
+++ weather/tasks.py
@@ -7,8 +7,8 @@ from decouple import config
 
 @shared_task
 def send_email_task():
-    send_mail('Celery Task Worked!',
-              'This is the proof the task worked!',
-              config('EMAIL'),
-              ['florin.danci96@gmail.com'])
+    # send_mail('Celery Task Worked!',
+    #           'This is the proof the task worked!',
+    #           config('EMAIL'),
+    #           ['florin.danci96@gmail.com'])
     return None
