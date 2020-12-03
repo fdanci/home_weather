22d62481bc51bce91d1823c27e7022f06df8c672
fdanci <florin.danci96@gmail.com>
2020-12-03 12:19:48 +0200

    Add alarm buttons and settings model

--- weather/models.py
+++ weather/models.py
@@ -10,3 +10,14 @@ class Forecast(models.Model):
 
     class Meta:
         verbose_name_plural = 'forecasts'
+
+
+class Settings(models.Model):
+    """Single entity holding information about worker beat interval."""
+
+    # The location where the forecast conditions are checked.
+    location = models.CharField(max_length=10, default='cosna')
+    # 1 - Monday, 2 - Tuesday, etc.
+    frequency = models.CharField(max_length=20, default='1')
+    # The hour at which the beat starts.
+    hour = models.CharField(max_length=20, default='08h00')
--- weather/tasks.py
+++ weather/tasks.py
@@ -4,9 +4,12 @@ from django.core.mail import send_mail
 
 from decouple import config
 
+from .models import Settings
+
 
 @shared_task
 def send_email_task():
+    # settings = Settings.objects.all()[0]
     # send_mail('Celery Task Worked!',
     #           'This is the proof the task worked!',
     #           config('EMAIL'),
